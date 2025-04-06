2ad4a5bb9ad05d818b133413d743fcc5544bbe94
Beatriz <168593598+monossacaridea@users.noreply.github.com>
2025-04-06 19:39:00 -0300

    Update Sistema_para_Agencia_de_Viagens.java

--- Sistema_para_Agencia_de_Viagens.java
+++ Sistema_para_Agencia_de_Viagens.java
@@ -1,2 +1,37 @@
 //Sistema para Agência de Viagens
 
+package projetoSistemaAgencia;
+
+import javax.swing.JOptionPane;
+
+public class Agencia {
+
+    public static void main(String[] args) {
+        Cliente c = new Cliente();  // Agora a classe Cliente é concreta
+        c.incluirCliente();
+    }
+}
+
+// Agora é uma classe concreta, não abstrata
+class Cliente {
+    String nome;
+    String CPF;
+    String passaporte;
+    String email;
+
+    public void incluirCliente() {
+        nome = JOptionPane.showInputDialog(null, "Insira seu nome:", "Cadastro", JOptionPane.QUESTION_MESSAGE);
+        CPF = JOptionPane.showInputDialog(null, "Insira seu CPF:", "Cadastro", JOptionPane.QUESTION_MESSAGE);
+        passaporte = JOptionPane.showInputDialog(null, "Insira seu passaporte:", "Cadastro", JOptionPane.QUESTION_MESSAGE);
+        email = JOptionPane.showInputDialog(null, "Insira seu email:", "Cadastro", JOptionPane.QUESTION_MESSAGE);
+    }
+}
+
+// Mantive como abstratas, já que você ainda não implementou nada nelas
+abstract class PacoteViagem {
+    // conteúdo futuro
+}
+
+abstract class ServicoAdicional {
+    // conteúdo futuro
+}
